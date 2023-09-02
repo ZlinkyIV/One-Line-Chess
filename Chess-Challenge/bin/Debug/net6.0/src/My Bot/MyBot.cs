2018e6da8e6fa0b5c003ce812e75a48cf7d9f792
Kaydon Sloan <zlinkyiv@gmail.com>
2023-09-02 13:05:45 -0400

    Negamax

--- Chess-Challenge/bin/Debug/net6.0/src/My Bot/MyBot.cs
+++ Chess-Challenge/bin/Debug/net6.0/src/My Bot/MyBot.cs
@@ -1,10 +1,77 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
+    int totalMovesSearched = 0;
+    int movesMade = 0;
+
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move move = board.GetLegalMoves()
+            .OrderByDescending(
+                move => board.MakeMove(move, board => -AlphaBeta(board, 3))
+            )
+            .FirstOrDefault();
+
+        movesMade += 1;
+
+        Console.WriteLine($"Total: {totalMovesSearched} \t Moves: {movesMade} \t Average: {totalMovesSearched / movesMade}");
+        
+        return move;
+    }
+
+    // int NegaMax(Board board, int depth) =>
+    //     board.GetLegalMoves().Length == 0
+    //         ? board.IsInCheckmate()
+    //             ? -1000000
+    //             : 0
+    //         : depth == 0
+    //             ? Evaluate(board)
+    //             : board.GetLegalMoves()
+    //                 .Select(move => board.MakeMove(move, board => -NegaMax(board, depth - 1)))
+    //                 .Max();
+
+    int AlphaBeta(Board board, int depth, int alpha = int.MinValue + 1, int beta = int.MaxValue) =>
+        board.GetLegalMoves().Length == 0
+            ? board.IsInCheckmate()
+                ? -1000000
+                : 0
+            : depth == 0
+                ? Evaluate(board)
+                : board.GetLegalMoves()
+                    .Aggregate(alpha, (alpha, move) =>
+                        alpha >= beta
+                            ? beta
+                            : Math.Max(
+                                alpha,
+                                board.MakeMove(move, board => -AlphaBeta(board, depth - 1, -beta, -alpha))
+                            )
+                    );
+
+    int Evaluate(Board board)
+    {
+        totalMovesSearched += 1;
+
+        return board.GetAllPieceLists()
+            .Sum(pieceList =>
+                new[] { 0, 100, 300, 300, 500, 900, 0 }[(int)pieceList.TypeOfPieceInList]
+                * pieceList.Count
+                * (pieceList.IsWhitePieceList ? 1 : -1)
+            )
+        * (board.IsWhiteToMove ? 1 : -1);
+    }
+}
+
+public static class Extensions
+{
+    public static T MakeMove<T>(this Board board, Move move, Func<Board, T> func)
+    {
+        board.MakeMove(move);
+        T value = func(board);
+        board.UndoMove(move);
+        return value;
     }
 }
